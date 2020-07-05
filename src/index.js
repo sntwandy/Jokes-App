de453a6961d5530960e50113b0bb365e19437bba
sntwandy <sntwandy@gmail.com>
2020-07-04 21:15:15 -0400

    Updating the fetch petitin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch(jokeUrl)
+/* fetch(jokeUrl)
     .then((response) => {
         response.json().then(data => {
             console.log(data);
         })
-    });
+}); */
+
+fetch(jokeUrl)
+    .then(response => response.json())
+    .then( ( { id, value } ) => console.log(id, value) );

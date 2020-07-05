e9909dbf3af1ba6240923008373013825abd5de1
sntwandy <sntwandy@gmail.com>
2020-07-04 23:54:57 -0400

    POST with Fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import *as CRUD from './js/crud-provider';
 
-CRUD.getUsers(1)
-    .then(console.log);
+/* CRUD.getUsers(1)
+    .then(console.log)
+    .catch(console.log); */
+
+CRUD.createUser({
+    name: 'Megan E.',
+    job: 'Criminology'
+}).then(console.log);
--- src/js/crud-provider.js
+++ src/js/crud-provider.js
@@ -1,12 +1,27 @@
 const urlCRUD = 'https://reqres.in/api/users';
 
 const getUsers = async (id) => {
-    const response = await fetch(`${urlCRUD}/${id}`);
-    const { data } = await response.json();
-
-    return data;
+    try {
+        const response = await fetch(`${urlCRUD}/${id}`);
+        const { data } = await response.json();
+        return data;
+    } catch(err) {
+        throw err;
+    }
 };
+
+const createUser = async (user) => {
+    const response = await fetch(urlCRUD, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    return await response.json();
+}
 
 export {
     getUsers,
+    createUser,
 }
